1ca25d507883fd90e4eaa279a247eaf673df5134
AkashHalayyanavar <75541750+AkashHalayyanavar@users.noreply.github.com>
2025-06-30 23:16:54 +0530

    Time: 6 ms (98.17%) | Memory: 47.7 MB (81.86%) - LeetSync

--- SolvdQuestions/49-group-anagrams/group-anagrams.java
+++ SolvdQuestions/49-group-anagrams/group-anagrams.java
@@ -1,45 +1,29 @@
 class Solution {
-    boolean anagram(String str1, String str2) {
-        if (str1.length() != str2.length())
-            return false;
-
-        HashMap<Character, Integer>  mp = new HashMap<>();
-
-        for (Character ch: str1.toCharArray()) {
-            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
-        }
-
-        for (Character ch: str2.toCharArray()) {
-            if (mp.get(ch) == null) 
-                return false;
-            mp.put(ch, mp.get(ch) - 1);
-            if(mp.get(ch) == 0) {
-                mp.remove(ch);
-            }
-        }
-        return true;
-    }
-
-    void checkAndAdd(String str, List<List<String>> result) {
-        for(int i=0; i<result.size(); i++) {
-            String resStr = result.get(i).get(0);
-            if (anagram(str, resStr)) {
-                result.get(i).add(str);
-                return;
-            }
-        }
-        List<String> res = new ArrayList<>();
-        res.add(str);
-        result.add(res);
-    }
 
     public List<List<String>> groupAnagrams(String[] strs) {
         
-        List<List<String>> result = new ArrayList<>();
+        List<List<String>> result;
+        Map<String, List<String>> mp = new HashMap<>();
 
         for (String str: strs) {
-            checkAndAdd(str, result);
+            // sort the string
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+            
+            // put the orignal string in the hashmap list with sortedStr as key
+            List<String> strList = mp.get(sortedStr);
+            // new group case
+            if (strList == null) {
+                strList = new ArrayList<>();
+                strList.add(str);
+                mp.put(sortedStr, strList);
+            } else {
+                strList.add(str);
+            }
         }
+
+        result = new ArrayList<>(mp.values());
 
         return result;
     }
