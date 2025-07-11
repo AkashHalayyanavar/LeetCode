d4854e3632205a3cd2545d85a283b097c4ce39cf
AkashHalayyanavar <75541750+AkashHalayyanavar@users.noreply.github.com>
2025-07-12 01:33:43 +0530

    Time: 1 ms (62.82%) | Memory: 46.6 MB (38.57%) - LeetSync

--- SolvdQuestions/42-trapping-rain-water/trapping-rain-water.java
+++ SolvdQuestions/42-trapping-rain-water/trapping-rain-water.java
@@ -2,26 +2,29 @@ class Solution {
     public int trap(int[] height) {
 
         int n = height.length;
-        int[] leftMax = new int[n];
-        int[] rightMax = new int[n];
-        int result = 0;
+        
+        int leftMax=0, rightMax=0, result=0;
+        int left=0, right=n-1;
 
-        // populate left max
-        // starting from me who is the maximum on left side
-        leftMax[0] = height[0];
-        for (int i=1; i<n; i++)
-            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        while (left < right) {
 
-        // populate right max
-        // starting from me who is the maximum on right side
-        rightMax[n-1] = height[n-1];
-        for (int i=n-2; i>=0; i--)
-            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+            // compute leftMax for left pointer
+            leftMax = Math.max(height[left], leftMax);
 
-        // Calculate the amount of water trapped above each bar
-        // There can be no water trapped above first and last bar
-        for (int i=1; i<n-1; i++)
-            result += Math.min(leftMax[i], rightMax[i]) - height[i];
+            // compute rightMax for right pointer
+            rightMax = Math.max(height[right], rightMax);
+
+            // leftMax is the deciding factor for left pointer index
+            if (leftMax < rightMax) {
+                result += (leftMax - height[left]);
+                left++;
+            }
+            // rightMax is the deciding factor for right pointer index
+            else {
+                result += (rightMax - height[right]);
+                right--;
+            }
+        }
         
         return result;
     }
