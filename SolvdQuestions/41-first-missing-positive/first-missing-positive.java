dc4280bdc58dc45381fbd80dd0166ec4199c5a56
AkashHalayyanavar <75541750+AkashHalayyanavar@users.noreply.github.com>
2025-07-25 00:43:19 +0530

    Time: 1 ms (100.00%) | Memory: 57 MB (73.59%) - LeetSync

--- SolvdQuestions/41-first-missing-positive/first-missing-positive.java
+++ SolvdQuestions/41-first-missing-positive/first-missing-positive.java
@@ -1,31 +1,34 @@
 class Solution {
+
+    void swap(int index1, int index2, int[] nums) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+
     public int firstMissingPositive(int[] nums) {
 
-        // Logic to be used: we will convert the array such a way that it contains only the values between 1 and n+1
-        // i.e., first by replacing non positive integers with value n+1
-
-        // Then we will traverse the array, and for any number val in range [1,n] we will update the index val-1 with negative value
-        // the -ve value indicating that the number is already present
-        // then in the final iteration, wherever we get the first index with pos value will give the required number
+        // Second Approach: Cycle sort
+        // For values in the range [1,n] swap with the values with their corresponding index
+        // so the values should be present at their index
+        // for eg: 1 at 0, 2 at 1, 3 at 2, ...., n at n-1
 
         int n = nums.length;
 
-        // Step1: set all non positive integers to n+1
-        for (int i=0; i<n; i++) {
-            if(nums[i] <= 0)
-                nums[i] = n+1;
+        int i=0;
+
+        while (i<n) {
+            // correct index for nums[i]
+            int correctIndex = nums[i] - 1;
+            if (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[correctIndex])
+                swap(i, correctIndex, nums);
+            else
+                i++;
         }
 
-        // Step2: start marking -ve for values in range [1,n]
-        for (int i=0; i<n; i++) {
-            int val = Math.abs(nums[i]);
-            if (val <= n)
-                nums[val-1] = -Math.abs(nums[val-1]);
-        }
-
-        // Step3: find the first missing positive, by checking the index of first +ve value
-        for (int i=0; i<n; i++) {
-            if (nums[i] > 0)
+        // check the first non matching index and value number
+        for (i=0; i<n; i++) {
+            if (nums[i] != i+1)
                 return i+1;
         }
 
