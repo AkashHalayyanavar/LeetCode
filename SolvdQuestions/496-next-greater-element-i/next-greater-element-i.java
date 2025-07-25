0c5997a2a65fe5d6f8082c0c7f7da6ce0e9cbd0f
AkashHalayyanavar <75541750+AkashHalayyanavar@users.noreply.github.com>
2025-07-25 11:17:46 +0530

    Time: 3 ms (92.47%) | Memory: 44.7 MB (7.13%) - LeetSync

--- SolvdQuestions/496-next-greater-element-i/next-greater-element-i.java
+++ SolvdQuestions/496-next-greater-element-i/next-greater-element-i.java
@@ -1,26 +1,32 @@
 class Solution {
-
-    int getNextGreater(int start, int end, int val, int[] arr) {
-
-        for (int i=start; i<end; i++) {
-            if (arr[i] > val)
-                return arr[i];
-        }
-
-        return -1;
-    }
-
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
         int m = nums1.length;
         int n = nums2.length;
 
         int[] result = new int[m];
 
-        for (int i=0; i<m; i++) {
-            for (int j=0; j<n; j++) {
-                if (nums1[i] == nums2[j]) 
-                    result[i] = getNextGreater(j+1, n, nums2[j], nums2);
+        Deque<Integer> st = new ArrayDeque<>();
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        // find out all the next greater elements in nums2
+        for (int i=n-1; i>=0; i--) {
+            while (!st.isEmpty() && nums2[i] > st.peek()) {
+                st.poll();
             }
+
+            // if stack is empty => no next greater element for this nums2[i]
+            if (st.isEmpty())
+                mp.put(nums2[i], -1);
+            else
+                mp.put(nums2[i], st.peek());
+
+            // push nums2[i] in the stack
+            st.push(nums2[i]);
+        }
+
+        for (int i=0; i<m; i++) {
+            result[i] = mp.get(nums1[i]);
         }
 
         return result;
