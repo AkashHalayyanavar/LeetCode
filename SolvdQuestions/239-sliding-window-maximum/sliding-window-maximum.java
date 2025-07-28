ed503c36d49fa79c79238c07b857d2504a194177
AkashHalayyanavar <75541750+AkashHalayyanavar@users.noreply.github.com>
2025-07-28 20:58:00 +0530

    Time: 27 ms (95.83%) | Memory: 59.5 MB (44.00%) - LeetSync

--- SolvdQuestions/239-sliding-window-maximum/sliding-window-maximum.java
+++ SolvdQuestions/239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,36 +1,44 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        
+
+        int n = nums.length;
+
+        // we will store index in the queue
         Deque<Integer> dq = new ArrayDeque<>();
 
-        List<Integer> result = new ArrayList<>();
+        int[] result = new int[n-k+1];
 
-        // first window
+        // first window of size k
         for (int i=0; i<k; i++) {
-
-            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i])
+                
+            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
                 dq.pollLast();
+            }
 
-
-            dq.addLast(i);
+            dq.offerLast(i);
         }
 
-        // subsequent windows
-        for (int i=k; i<nums.length; i++) {
-            result.add(nums[dq.peekFirst()]);
+        // NOTE: Max will always be at the front of the queue
 
-            if(dq.peekFirst() <= (i-k))
+        // Add the first window result
+        result[0] = nums[dq.peekFirst()];
+
+        // keep moving the index till we reach the end
+        for (int i=k; i<n; i++) {
+
+            // remove the first index if it is not part of the current window
+            if (dq.peekFirst() == (i-k) ) 
                 dq.pollFirst();
 
-            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i])
+            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
                 dq.pollLast();
+            }
 
-            dq.addLast(i);
+            dq.offerLast(i);
+            // Add result for the current window
+            result[i-k+1] = nums[dq.peekFirst()];
         }
-
-        result.add(nums[dq.peekFirst()]);
-
-        // convert list to int[] and return
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        
+        return result;
     }
 }
