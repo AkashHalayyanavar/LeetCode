0e4dc093da4d0a198e34b69e0bc3cc1ee15c266e
AkashHalayyanavar <75541750+AkashHalayyanavar@users.noreply.github.com>
2025-08-08 20:20:11 +0530

    Time: 6 ms (63.47%) | Memory: 45.3 MB (37.72%) - LeetSync

--- SolvdQuestions/826-soup-servings/soup-servings.java
+++ SolvdQuestions/826-soup-servings/soup-servings.java
@@ -1,23 +1,23 @@
 class Solution {
-    double solve(int x, int y, HashMap<String, Double> mp) {
+    double solve(int x, int y, HashMap<Pair, Double> mp) {
 
         if (x <=0 && y<=0) {
-            mp.put(x + "#" + y, 0.5);
+            mp.put(new Pair(x,y), 0.5);
             return 0.5;
         }
 
         if (x <= 0) {
-            mp.put(x + "#" + y, 1.0);
+            mp.put(new Pair(x,y), 1.0);
             return 1;
         }
 
         if (y <= 0) {
-            mp.put(x + "#" + y, 0.0);
+            mp.put(new Pair(x,y), 0.0);
             return 0;
         }
 
-        if (mp.containsKey(x + "#" + y)) {
-            return mp.get(x + "#" + y);
+        if (mp.containsKey(new Pair(x,y))) {
+            return mp.get(new Pair(x,y));
         } 
             
         double op1 = solve(x-100, y, mp);
@@ -26,7 +26,7 @@ class Solution {
         double op4 = solve(x-25, y-75, mp);
 
         double result = 0.25 * (op1 + op2 + op3 + op4);
-        mp.put(x + "#" + y, result);
+        mp.put(new Pair(x,y), result);
 
         return result;
     }
@@ -35,7 +35,7 @@ class Solution {
         if (n > 5000)
             return 1;
 
-        HashMap<String, Double> mp = new HashMap<>();
+        HashMap<Pair, Double> mp = new HashMap<>();
         return solve(n, n, mp);
         
     }
