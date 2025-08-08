53937ba3c33c66805b90cafad6232348b528b4f2
AkashHalayyanavar <75541750+AkashHalayyanavar@users.noreply.github.com>
2025-08-08 20:17:41 +0530

    Time: 18 ms (31.14%) | Memory: 46.4 MB (32.93%) - LeetSync

---
+++ SolvdQuestions/826-soup-servings/soup-servings.java
@@ -0,0 +1,42 @@
+class Solution {
+    double solve(int x, int y, HashMap<String, Double> mp) {
+
+        if (x <=0 && y<=0) {
+            mp.put(x + "#" + y, 0.5);
+            return 0.5;
+        }
+
+        if (x <= 0) {
+            mp.put(x + "#" + y, 1.0);
+            return 1;
+        }
+
+        if (y <= 0) {
+            mp.put(x + "#" + y, 0.0);
+            return 0;
+        }
+
+        if (mp.containsKey(x + "#" + y)) {
+            return mp.get(x + "#" + y);
+        } 
+            
+        double op1 = solve(x-100, y, mp);
+        double op2 = solve(x-75, y-25, mp);
+        double op3 = solve(x-50, y-50, mp);
+        double op4 = solve(x-25, y-75, mp);
+
+        double result = 0.25 * (op1 + op2 + op3 + op4);
+        mp.put(x + "#" + y, result);
+
+        return result;
+    }
+
+    public double soupServings(int n) {
+        if (n > 5000)
+            return 1;
+
+        HashMap<String, Double> mp = new HashMap<>();
+        return solve(n, n, mp);
+        
+    }
+}
