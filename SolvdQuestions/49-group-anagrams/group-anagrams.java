08f6a4fa017c102915b65ce8db45c058b62ba031
AkashHalayyanavar <75541750+AkashHalayyanavar@users.noreply.github.com>
2025-08-09 23:50:45 +0530

    Time: 6 ms (98.56%) | Memory: 48 MB (54.93%) - LeetSync

--- SolvdQuestions/49-group-anagrams/group-anagrams.java
+++ SolvdQuestions/49-group-anagrams/group-anagrams.java
@@ -1,42 +1,20 @@
 class Solution {
-
     public List<List<String>> groupAnagrams(String[] strs) {
-        
-        List<List<String>> result;
-        Map<String, List<String>> mp = new HashMap<>();
+    
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chArr = new char[26];
+            for (char ch : s.toCharArray()) 
+                chArr[ch - 'a']++;
 
-        for (String str: strs) {
+            String keyStr = String.valueOf(chArr);
+
+            if (!map.containsKey(keyStr)) 
+                map.put(keyStr, new ArrayList<>());
             
-            int[] counts = new int[26];
-
-            // fill the counts array for the string str
-            for (char ch: str.toCharArray()) {
-                counts[ch-'a']++;
-            } 
-
-            // Create a unique string signature based on counts
-            StringBuilder sb = new StringBuilder();
-            for (int count: counts) {
-                // Use '#' to separate counts for each letter
-                sb.append(count).append("#"); 
-            }
-
-            String sortedStr = sb.toString();
-
-            // put the orignal string in the hashmap list with sortedStr as key
-            List<String> strList = mp.get(sortedStr);
-            // new group case
-            if (strList == null) {
-                strList = new ArrayList<>();
-                strList.add(str);
-                mp.put(sortedStr, strList);
-            } else {
-                strList.add(str);
-            }
+            map.get(keyStr).add(s);
         }
-
-        result = new ArrayList<>(mp.values());
-
-        return result;
+        
+        return new ArrayList<>(map.values());
     }
 }
