140b438e70d6dbe0d65180b92cda4fe5ffeec7a5
AkashHalayyanavar <75541750+AkashHalayyanavar@users.noreply.github.com>
2025-08-10 11:06:36 +0530

    Time: 12 ms (82.69%) | Memory: 45.6 MB (88.73%) - LeetSync

--- SolvdQuestions/347-top-k-frequent-elements/top-k-frequent-elements.java
+++ SolvdQuestions/347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -7,6 +7,7 @@ class Solution {
             mp.put(num, mp.getOrDefault(num, 0) + 1);
         }
 
+        // Integer.compare(b.getValue(), a.getValue()) we can also use this, both are same
         PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>( (a,b) -> (b.getValue() - a.getValue()) );
 
         // put entries to the heap
