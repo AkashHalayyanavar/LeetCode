75f542e0c3a8b9d02896dfcfabb9db965937c539
AkashHalayyanavar <75541750+AkashHalayyanavar@users.noreply.github.com>
2025-09-07 01:10:37 +0530

    Time: 67 ms (36.00%) | Memory: 75.7 MB (57.92%) - LeetSync

--- SolvdQuestions/1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
+++ SolvdQuestions/1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
@@ -1,41 +1,47 @@
 class Solution {
     public int maxEvents(int[][] events) {
 
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        int maxDay = Arrays.stream(events).mapToInt(event -> event[1]).max().getAsInt();
+        // sort the events in the ascending order based on the start time
+        // if the start times are same then based on their end times
+        Arrays.sort(events, (a,b) -> {
+            if (a[0] != b[0])
+                return a[0] - b[0];
 
-        System.out.println(maxDay);
+            return a[1] - b[1];
+        });
 
-        int result = 0;
-        int n = events.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        // Default sort 
-        Arrays.sort(events, Comparator.comparingInt(a -> a[0]));
+        int currDay=1;
+        int i = 0;
+        int attended = 0;
 
-        // Start from day 1 and go till maxDay
-        for (int i=1, j=0; i<=maxDay; i++) {
-            // All the events whose starting day is less than equal to day i add to min heap
-            // since those are the events which might be possible to be assigned on day i
-            while (j<n && events[j][0] <= i) {
-                pq.offer(events[j][1]);
-                j++;
+        // dummy entry
+        pq.offer(-1);
+
+        while (i<events.length || !pq.isEmpty()) {
+
+            // Add all the events end dates to pq, whoes start date is less than or equal to currDate
+            while (i<events.length && events[i][0] <= currDay) {
+                pq.offer(events[i][1]);
+                i++;
             }
 
-            // Remove from heap all the meetings whose end day is less than i
-            // As they can no longer be attended
-            while(!pq.isEmpty() && pq.peek() < i) {
+            // Remove all the endDates less than i as they can no longer be attended
+            while (!pq.isEmpty() && pq.peek() < currDay)
                 pq.poll();
-            }
 
-            // If the heap is not empty, we attend the meeting with the earliest end time
-            // (which is at the top of the heap), increment the count of attended meetings by 1, 
-            // and remove it from the heap
+            // Attend one event with the least end date (top of pq)
             if (!pq.isEmpty()) {
+                attended++;
                 pq.poll();
-                result++;
             }
+
+            // move to next day
+            currDay++;
         }
 
-        return result;
+        return attended;
+        
     }
 }
