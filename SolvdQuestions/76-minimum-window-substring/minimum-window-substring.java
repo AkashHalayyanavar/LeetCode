c7be16abe4a0acc1f97782d39a190b62dbeaec32
AkashHalayyanavar <75541750+AkashHalayyanavar@users.noreply.github.com>
2025-09-06 16:53:15 +0530

    Time: 16 ms (68.00%) | Memory: 45 MB (60.35%) - LeetSync

--- SolvdQuestions/76-minimum-window-substring/minimum-window-substring.java
+++ SolvdQuestions/76-minimum-window-substring/minimum-window-substring.java
@@ -2,53 +2,45 @@ class Solution {
     public String minWindow(String s, String t) {
         
         Map<Character, Integer> mp = new HashMap<>();
+        int count = t.length();
+        int startIndex = 0;
+        int minLen = Integer.MAX_VALUE;
 
-        // store the t characters into the map
-        for (Character ch: t.toCharArray()) {
-            mp.put(ch, mp.getOrDefault(ch, 0)+1);
+        // Add all the characters of t to the map
+        for (char ch: t.toCharArray()) {
+            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
         }
 
-        int left=0, right=0;
-        int minLen=Integer.MAX_VALUE;
-        int resultStartIndex = 0;
+        // For the string s go from left to right
+        int i=0, j=0;
+        while (j < s.length()) {
+            char ch = s.charAt(j);
+            if (mp.containsKey(ch)) {
 
-        int count = t.length();
-
-        while (right < s.length()) {
-            if (mp.containsKey(s.charAt(right))) {
-                int val = mp.get(s.charAt(right));
-                if (val > 0) {
+                if (mp.get(ch) > 0) {
                     count--;
                 }
-                mp.put(s.charAt(right), val-1);
+                mp.put(ch, mp.get(ch)-1);
             }
 
-            // if count is 0, then keep moving left till we see the count is increased
-            while (count==0) {
-                // check if minLen is lesser now
-                if (right-left+1 < minLen) {
-                    resultStartIndex = left;
-                    minLen = right-left+1;
+            while (count == 0) {
+                if ( minLen > j-i+1) {
+                    minLen = j-i+1;
+                    startIndex = i;
                 }
 
-
-                // see if the left element is part of target
-                if (mp.containsKey(s.charAt(left))) {
-                    // increment the counter in map, as we are moving left
-                    int val = mp.get(s.charAt(left));
-                    mp.put(s.charAt(left),  val+ 1);
+                if (mp.containsKey(s.charAt(i))) {
+                    int val = mp.get(s.charAt(i));
+                    mp.put(s.charAt(i), val + 1);
                     if (val >= 0)
                         count++;
                 }
-                left++;
+                i++;
             }
 
-            right++;
+            j++;
         }
 
-        System.out.println(resultStartIndex);
-
-        return minLen == Integer.MAX_VALUE ? new String() 
-                        : s.substring(resultStartIndex, resultStartIndex+minLen);
+        return minLen == Integer.MAX_VALUE ? new String() : s.substring(startIndex, startIndex+minLen);
     }
 }
