442aaba0844e728a0fa59cb9ca30efad848dd683
AkashHalayyanavar <75541750+AkashHalayyanavar@users.noreply.github.com>
2025-10-26 14:04:15 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (48.03%) - LeetSync

--- SolvdQuestions/1184-car-pooling/car-pooling.java
+++ SolvdQuestions/1184-car-pooling/car-pooling.java
@@ -1,27 +1,24 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        
-        TreeMap<Integer, Integer> mp = new TreeMap<>();
+
+        int[] prefixArray = new int[1001];
 
         for (int[] trip: trips) {
-            int num = trip[0];
             int start = trip[1];
             int end = trip[2];
 
-            mp.put(start, mp.getOrDefault(start, 0) + num);
-            mp.put(end, mp.getOrDefault(end, 0) - num);
+            prefixArray[start] += trip[0];
+            prefixArray[end] -= trip[0];
         }
 
-        // iterate over the map and compute the prefix sum
-        // see if at any point of time the num of passengers exceeds the capacity
-        int sum = 0;
-        for (int value: mp.values()) {
-            sum += value;
-
+        // form the prefix array and see if any where the capacity is violated
+        int sum = prefixArray[0];
+        for (int i=1; i<1001; i++) {
             if (sum > capacity)
                 return false;
+            sum += prefixArray[i];
         }
-
+        
         return true;
     }
 }
