40016502275d028e231b537eb6e59a3a2a5aaebd
AkashHalayyanavar <75541750+AkashHalayyanavar@users.noreply.github.com>
2025-10-26 13:09:50 +0530

    Time: 6 ms (60.26%) | Memory: 45 MB (10.88%) - LeetSync

--- SolvdQuestions/139-word-break/word-break.java
+++ SolvdQuestions/139-word-break/word-break.java
@@ -1,35 +1,36 @@
 class Solution {
-    Boolean[] dp;
-    boolean helper(String s, int pos, Set<String> set) {
-        int n = s.length();
-        // base case
-        if(pos == n)
+
+    boolean isPossible(String s, int index, int n, HashSet<String> set, Boolean[] dp) {
+
+        if (index == n)
             return true;
 
-        if (dp[pos]!=null)
-            return dp[pos];
+        if (dp[index] != null)
+            return dp[index];
 
-        for (int i=pos; i<n; i++) {
-            if(set.contains(s.substring(pos, i+1)) && helper(s, i+1, set) ) {
-                dp[pos] = true;
+        for (int i=index+1; i<=n; i++) {
+            String sub = s.substring(index, i);
+            if (set.contains(sub) && isPossible(s, i, n, set, dp)) {
+                dp[index] = true;
                 return true;
             }
         }
 
-        dp[pos] = false;
+        dp[index] = false;
         return false;
     }
 
-    
     public boolean wordBreak(String s, List<String> wordDict) {
 
-        // Store the wordDict words in the set
-        Set<String> set = new HashSet<>(wordDict);
-
         int n = s.length();
-        dp = new Boolean[n];
+        HashSet<String> set = new HashSet<>();
+        Boolean[] dp = new Boolean[n];
 
-        return helper(s, 0, set);
-        
+        // Store words ito the hash set
+        for (String word: wordDict)
+            set.add(word);
+
+
+        return isPossible(s, 0, n, set, dp);
     }
 }
