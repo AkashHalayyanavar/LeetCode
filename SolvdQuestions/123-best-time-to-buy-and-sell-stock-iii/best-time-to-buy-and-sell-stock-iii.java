2128b1b54ed3e358c960c0a591279c549f1b99e5
AkashHalayyanavar <75541750+AkashHalayyanavar@users.noreply.github.com>
2026-03-14 17:01:21 +0530

    Time: 246 ms (5.04%) | Memory: 204 MB (5.00%) - LeetSync

---
+++ SolvdQuestions/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    int getMaxProfit(int[] prices, int index, int buy, int transactions, Integer[][][] dp) {
+
+        if (index == prices.length || transactions == 0)
+            return 0;
+
+        if (dp[index][buy][transactions] != null)
+            return dp[index][buy][transactions];
+
+        // if (transactions == 0) {
+        //     dp[index][buy][transactions] = 0;
+        //     return dp[index][buy][transactions];
+        // }
+
+        // buy=1 means buy, buy=0 means sell
+        if (buy == 1) {
+
+            // either buy at index or go to next index without buying
+            dp[index][buy][transactions] = Math.max(
+                -prices[index] + getMaxProfit(prices, index+1, 0, transactions, dp),
+                getMaxProfit(prices, index+1, 1, transactions, dp)
+            );
+        } else if (buy == 0) {
+
+            // either sell at index or go to next index without selling
+            dp[index][buy][transactions] = Math.max(
+                prices[index] + getMaxProfit(prices, index+1, 1, transactions-1, dp),
+                getMaxProfit(prices, index+1, 0, transactions, dp)
+            );
+        }
+
+        return dp[index][buy][transactions];
+    }
+
+    public int maxProfit(int[] prices) {
+        Integer[][][] dp = new Integer[prices.length][2][3];
+        return getMaxProfit(prices, 0, 1, 2, dp);
+    }
+}
